37bb89343fd910e1aa710faa3b5475e7e5d9c527
Varenyam Kaushik <varenyamkaushik@yahoo.com>
2016-08-04 22:36:56 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/varenyam/gitexample2/GitExample2ActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.varenyam.gitexample2;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GitExample2ActivityFragment extends Fragment {
+
+    public GitExample2ActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_git_example2, container, false);
+    }
+}
